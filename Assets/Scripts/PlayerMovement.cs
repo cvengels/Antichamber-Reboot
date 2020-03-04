23770c499bef709083bb77191521cc41d3eeb697
C.Vengels <6281096+cvengels@users.noreply.github.com>
2020-03-04 05:31:02 +0100

    Init

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float moveSpeed = 12f;
+    public float gravity = -9.81f;
+
+    Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+
+        Vector3 move = Vector3.Normalize(transform.right * xMove + transform.forward * zMove);
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
