e631b4682cd9a1b755134b8ea53d738826f5185e
C.Vengels <6281096+cvengels@users.noreply.github.com>
2020-03-04 13:33:11 +0100

    Added preview cube

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MouseLook : MonoBehaviour
@@ -7,6 +8,14 @@ public class MouseLook : MonoBehaviour
     public float mouseSensitivity = 1000f;
 
     public Transform playerBody;
+
+    RaycastHit hit;
+    Ray forwardRay;
+    public Transform previewCube;
+
+    public TextMeshProUGUI xAngleTextUI;
+    public TextMeshProUGUI yAngleTextUI;
+    public TextMeshProUGUI zAngleTextUI;
 
     float xRotation = 0f;
 
@@ -26,5 +35,20 @@ public class MouseLook : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+
+        // Preview Cube
+        forwardRay = new Ray(transform.position, transform.forward);
+
+        if (Physics.Raycast(forwardRay, out hit) && Input.GetMouseButton(0))
+        {
+            xAngleTextUI.text = "X: " + (int)hit.normal.x;
+            yAngleTextUI.text = "Y: " + (int)hit.normal.y;
+            zAngleTextUI.text = "Z: " + (int)hit.normal.z;
+
+            Debug.Log(hit.point.ToString());
+            previewCube.position = new Vector3( (Mathf.Round(hit.point.x / 0.2f) * 0.2f) + ((int)hit.normal.x * previewCube.transform.localScale.x / 2), 
+                                                (Mathf.Round(hit.point.y / 0.2f) * 0.2f) + ((int)hit.normal.y * previewCube.transform.localScale.y / 2), 
+                                                (Mathf.Round(hit.point.z / 0.2f) * 0.2f) + ((int)hit.normal.z * previewCube.transform.localScale.z / 2));
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,16 +8,34 @@ public class PlayerMovement : MonoBehaviour
 
     public float moveSpeed = 12f;
     public float gravity = -9.81f;
+    public float jumpHeight = 3f;
 
     Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float xMove = Input.GetAxis("Horizontal");
         float zMove = Input.GetAxis("Vertical");
 
-        Vector3 move = Vector3.Normalize(transform.right * xMove + transform.forward * zMove);
+        Vector3 move = transform.right * xMove + transform.forward * zMove;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         controller.Move(move * moveSpeed * Time.deltaTime);
 
